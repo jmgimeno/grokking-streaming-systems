0104afc834b9e6f530e4cf7852f53ad56620081a
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2022-07-15 09:05:10 +0200

    changed to a ConcurrentHashMap to avoid race conditions

--- src/main/java/com/streamwork/ch04/job/ScoreStorage.java
+++ src/main/java/com/streamwork/ch04/job/ScoreStorage.java
@@ -1,22 +1,22 @@
 package com.streamwork.ch04.job;
 
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * ScoreStorage is a class to simulate a very basic key-value store using a hashmap object.
  */
 class ScoreStorage implements Serializable {
   private static final long serialVersionUID = -1157146960334381327L;
-  Map<String, Float> transactionScores = new HashMap<>();
+  Map<String, Float> transactionScores = new ConcurrentHashMap<>();
 
   public ScoreStorage() {}
 
   /**
    * Get the score of a transaction.
    * @param transaction
-   * @param default
+   * @param defaultValue
    * @return
    */
   float get(String transaction, float defaultValue) {
